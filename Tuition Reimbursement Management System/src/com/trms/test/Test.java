0968d8707220aa2b0670a402de877457f40f0cb2
woodbry <woodbry@mail.gvsu.edu>
2019-11-01 12:03:54 -0400

    added js activity

--- Tuition Reimbursement Management System/src/com/trms/test/Test.java
+++
@@ -1,5 +0,0 @@
-package com.trms.test;
-
-public class Test {
-
-}
---
+++ web thing/explode.js
@@ -0,0 +1,19 @@
+
+
+window.onload= function(){
+    document.getElementById("button").addEventListener("click",play);
+    
+}
+
+onclick="document.getElementById('img').src='explosion.gif'"
+
+function play(){
+     var audio = document.getElementById("audio");
+     audio.play();
+               }
+ 
+
+input ="button";
+value="PLAY";
+onclick="play()";
+audio = "zapsplat_explosion_designed_big_distorted_fire_backdraft_001_31097.mp3";
