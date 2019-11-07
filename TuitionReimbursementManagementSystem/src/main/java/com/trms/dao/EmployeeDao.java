773282705a3e6b8bc3249e3c537aca76c3efd0f1
woodbry <woodbry@mail.gvsu.edu>
2019-11-06 19:49:31 -0500

    update new project

--- TuitionReimbursementManagementSystem/src/main/java/com/trms/dao/EmployeeDao.java
+++ TuitionReimbursementManagementSystem/src/main/java/com/trms/dao/EmployeeDao.java
@@ -6,15 +6,19 @@ import java.util.List;
 import com.trms.beans.Employee;
 
 public interface EmployeeDao {
-public List<Employee> getAllEmployees() throws SQLException;
+
+	public List<Employee> getAllEmployees() throws SQLException;
 
 	public Employee getEmployeeByFId(int formId) throws SQLException;
 
 	public Employee getEmployeeByEId(int empId) throws SQLException;
 
-	public void addForm(Employee e) throws SQLException;
+	public void addEmployee(Employee e) throws SQLException;
 
-	public void updateForm(Employee e) throws SQLException;
+	public void updateEmployee(Employee e) throws SQLException;
 
-	public void removeForm(Employee e) throws SQLException;
+	public void removeEmployee(Employee e) throws SQLException;
+
+
+
 }
--- TuitionReimbursementManagementSystem/src/main/java/com/trms/dao/FormsDao.java
+++ TuitionReimbursementManagementSystem/src/main/java/com/trms/dao/FormsDao.java
@@ -8,14 +8,14 @@ import com.trms.beans.Forms;
 public interface FormsDao {
 
 	public List<Forms> getAllForms() throws SQLException;
-
+	
 	public Forms getFormByFId(int formId) throws SQLException;
-
+	
 	public List<Forms> getFormByEId(int empId) throws SQLException;
-
+	
 	public void addForm(Forms f) throws SQLException;
-
+	
 	public void updateForm(Forms f) throws SQLException;
-
+	
 	public void removeForm(Forms f) throws SQLException;
 }
